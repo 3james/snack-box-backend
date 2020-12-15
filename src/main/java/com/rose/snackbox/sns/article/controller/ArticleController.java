d68053481e87bf4ab090f80ada85147d75016ab7
3james <3james@naver.com>
2020-12-16 00:18:39 +0900

    삭제 메소드 추가

--- src/main/java/com/rose/snackbox/sns/article/controller/ArticleController.java
+++ src/main/java/com/rose/snackbox/sns/article/controller/ArticleController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,10 +23,10 @@ public class ArticleController {
     	log.debug("Get Article List Started !!");
 		
     	List<Article> articleList = articleService.getArticleList();
-		for (Article entity : articleList) {
-			log.debug(entity.getArticleId().toString());
-			log.debug(entity.getTitle());
-			log.debug(entity.getContent());
+		for (Article article : articleList) {
+			log.debug(article.getArticleId().toString());
+			log.debug(article.getTitle());
+			log.debug(article.getContent());
 		}
 
 		log.debug("Get Article List Ended !!");
@@ -38,20 +35,33 @@ public class ArticleController {
 	}	
 	
 	@PostMapping(value="/api/article")
-	public Article createArticle(
+	public ResponseEntity<?> createArticle(
 			@RequestParam("title") String title,
-			@RequestParam("content") String content	) {
+			@RequestParam("content") String content) {
 
 		log.debug("Create Article Started !!");
 		
-		Article articleEntity = new Article();
-		articleEntity.setTitle(title);
-		articleEntity.setContent(content);
-		Article returnArticleEntity = articleService.createArticle(articleEntity);
+		Article article = new Article();
+		article.setTitle(title);
+		article.setContent(content);
+		Article returnArticleEntity = articleService.createArticle(article);
 
 		log.debug("Create Article Ended !!");
 		
-		return returnArticleEntity;
+		return new ResponseEntity<Article>(returnArticleEntity, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value="/api/article/{articleId}")
+	public ResponseEntity<?> removeArticle(
+			@PathVariable("articleId") String articleId) {
+
+		log.debug("Remove Article Started !!");
+
+		articleService.removeArticle(Long.parseLong(articleId));
+
+		log.debug("Remove Article Ended !!");
+
+		return ResponseEntity.ok(HttpStatus.OK);
 	}
 	
 }
