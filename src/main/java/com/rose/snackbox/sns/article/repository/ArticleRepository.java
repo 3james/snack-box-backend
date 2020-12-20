08df7f749ce2a599639a9d3a49a0e28dc2957fc8
3james <3james@naver.com>
2020-12-20 12:14:41 +0900

    전체 목록 조회 시 정렬하는 메소드 추가

--- src/main/java/com/rose/snackbox/sns/article/repository/ArticleRepository.java
+++ src/main/java/com/rose/snackbox/sns/article/repository/ArticleRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rose.snackbox.sns.article.entity.Article;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    public List<Article> findAllByOrderByCreatedDateDesc();
 
 }
