8a2343beff337ce9f7d669f9cb60e02535d9114d
3james <3james@naver.com>
2020-12-20 12:23:02 +0900

    주석추가

--- src/main/java/com/rose/snackbox/sns/article/repository/ArticleRepository.java
+++ src/main/java/com/rose/snackbox/sns/article/repository/ArticleRepository.java
@@ -9,6 +9,10 @@ import java.util.Optional;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
+    /**
+     * article 전체 목록을 생성일자를 기준으로 오름차순으로 정렬하여 조회한다.
+     * @return article list
+     */
     public List<Article> findAllByOrderByCreatedDateDesc();
 
 }
